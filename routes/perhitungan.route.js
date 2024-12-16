82ae1bd767383cb68e26566ff650f90181097c1f
Joshtri <stuffofyos1516@gmail.com>
2024-12-16 18:16:45 +0800

    fixing royte

--- routes/perhitungan.route.js
+++ routes/perhitungan.route.js
@@ -2,11 +2,13 @@ import { getTOPSISResult } from '../controllers/demo.controller.js';
 import * as perhitunganController from '../controllers/perhitungan.controller.js';
 import express from 'express';
 
+import * as perhitunganController2 from '../controllers/perhitunganDemo.controller.js'
+
 const router = express.Router();
 
 
 router.get('/main_topsis', perhitunganController.MainPerhitunganPage);
-router.get('/normalized-matrix', perhitunganController.normalizedMatrixPage);
+router.get('/normalized-matrix', perhitunganController2.normalizedMatrixPage);
 router.post('/simpan-perhitungan', perhitunganController.saveHasilPerhitungan)
 router.get('/weighted-normalized-matrix', perhitunganController.weightedNormalizedMatrixPage)
 router.get('/data/hasil_perhitungan', perhitunganController.hasilPerhitunganPage)
@@ -14,5 +16,9 @@ router.post("/data/delete_hasil_perhitungan/:id", perhitunganController.deleteHa
 
 router.get('/demo', getTOPSISResult)
 // router.delete('/perhitungan')
+
+
+router.get('/data/export_pdf', perhitunganController.exportPDFByPeriode);
+
 
 export default router;
